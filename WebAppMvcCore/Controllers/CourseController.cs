8f1a647c4df95ac1a5da863bf5e7e2b6c7faad96
shraktim <shahadathossainraktim@gmail.com>
2018-11-18 22:07:16 +0600

    Create controller view

---
+++ WebAppMvcCore/Controllers/CourseController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebAppMvcCore.DatabaseContext;
+using WebAppMvcCore.Models;
+
+namespace WebAppMvcCore.Controllers
+{
+    public class CourseController : Controller
+    {
+        private ApplicationDbContext db;
+
+        public CourseController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Course model)
+        {
+            db.Courses.Add(model);
+            bool isSaved = db.SaveChanges() > 0;
+            if (isSaved)
+            {
+                ViewBag.Message = "Saved Succesful!";
+            }
+            ModelState.Clear();
+            return View();
+        }
+    }
+}
