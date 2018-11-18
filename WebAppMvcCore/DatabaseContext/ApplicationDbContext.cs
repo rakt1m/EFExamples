8315f3543cd2eae3228ea3e58a4d55faf4695dd2
shraktim <shahadathossainraktim@gmail.com>
2018-11-18 20:59:11 +0600

    Add DbContext

---
+++ WebAppMvcCore/DatabaseContext/ApplicationDbContext.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebAppMvcCore.Models;
+
+namespace WebAppMvcCore.DatabaseContext
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext()
+        {
+            
+        }
+
+        public ApplicationDbContext(DbContextOptions options):base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionBuilder)
+        {
+            optionBuilder.UseSqlServer("Server=RAKTIM-PC;Database=TrainingCenter;Trusted_Connrction=True");
+        }
+
+        public DbSet<Course> TrainingCenter { get; set; }
+    }
+}
---
+++ WebAppMvcCore/Models/Course.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAppMvcCore.Models
+{
+    public class Course
+    {
+        public Course()
+        {
+                
+        }
+
+        public Course( string courseName,string courseCode,string trainer,string time)
+        {
+            CourseName = courseName;
+            CourseCode = courseCode;
+            Trainer = trainer;
+            Time = time;
+        }
+        public int Id { get; set; }
+        public string CourseName { get; set; }
+        public string CourseCode { get; set; }
+        public string Trainer { get; set; }
+        public string Time { get; set; }
+    }
+}
